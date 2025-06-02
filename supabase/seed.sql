c274c191e39534ab32820f05b702bf32353c854f
fractal161 <628snapdragon@gmail.com>
2025-06-02 17:53:45 -0500

    initial schema

---
+++ supabase/seed.sql
@@ -0,0 +1,9 @@
+INSERT INTO public.users
+    (username, name)
+VALUES
+    ('shoji', 'Shoji Meguro'),
+    ('kkondo', 'Koji Kondo'),
+    ('vgo', 'VGO'),
+    ('fractal161', 'Fractal Onesixone'),
+    ('ianbgm', 'Big Man'),
+    ('wardle', 'Brian Wardle');
