2cc81f8e59a3414aa94ab914eab83f0399f4811f
fractal161 <628snapdragon@gmail.com>
2025-06-02 22:11:19 -0500

    add default availabilities in seed

--- supabase/seed.sql
+++ supabase/seed.sql
@@ -7,3 +7,11 @@ VALUES
     ('fractal161', 'Fractal Onesixone'),
     ('ianbgm', 'Big Man'),
     ('wardle', 'Brian Wardle');
+
+INSERT INTO public.weekly_availabilities
+    (user_id, start_day, availability)
+VALUES
+    (1, '2025-06-01', REPEAT('0', 336)),
+    (2, '2025-06-01', REPEAT('1', 336)),
+    (3, '2025-06-01', REPEAT('2', 336)),
+    (4, '2025-06-01', REPEAT(REPEAT('0', 24) || REPEAT('1', 24), 7));
