1bbc013518b5917a9437b6c44be04c20e4846e37
Lynn Jung <135656796+hlynnj@users.noreply.github.com>
2025-08-04 14:11:12 -0400

    change submit api call with db change

--- src/app/api/upsert-availability/route.ts
+++ src/app/api/upsert-availability/route.ts
@@ -7,27 +7,16 @@ const supabase = createClient(
 )
 
 export async function POST(req: Request) {
-    const { username, name, available, if_needed} = await req.json()
+    const { username, available, if_needed} = await req.json()
     var { data, error } = await supabase
         .from('user_availability')
         .upsert([{username, available, if_needed}], {
             onConflict: 'username',
         });
-    
-    if (error) {
-        return NextResponse.json({error: error.message}, {status: 500});
-    }
-
-    const last_availability_update = new Date().toISOString();
-    var { data, error } = await supabase
-        .from('users')
-        .upsert([{username, name, last_availability_update}], {
-            onConflict: 'username',
-        });
 
     if (error) {
         return NextResponse.json({error: error.message}, {status: 500});
     }
+
     return NextResponse.json({data})
-
 }
